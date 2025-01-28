b016911e9f94bbe860506665f390694b217d9fb6
coja <jovanvuceljic@gmail.com>
2025-01-28 12:12:04 +0100

    Cam selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Webcam from "react-webcam"
 
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  }
+
 function App() {
+  const [selectedDevice, setSelectedDevice] = useState(null);
+  const [devices, setDevices] = useState([]);
+
+
+  useEffect(() => {
+    (async () => {   
+    await navigator.mediaDevices.getUserMedia({audio: false, video: true});   
+    setDevices(await navigator.mediaDevices.enumerateDevices());
+    console.log(devices); 
+    })();
+  }, []);
+
+  console.log("devices", devices);
+  console.log("selectedDevice", selectedDevice);
+
+  const handleClick = (device) => {
+    console.log("clicked device", device);
+    setSelectedDevice(device.deviceId);
+  }
 
   return (
-    <main className="container">
-      <Webcam />
-    </main>
+    <div className="container">
+      <ul>
+        {devices.map((device) => {
+          const selected = selectedDevice?.deviceId === device.deviceId;
+          return (
+            <li
+              key={device.deviceId}
+              style={(selected && { color: "green" }) || {}}
+              onClick={() => handleClick(device)}
+            >
+              {device.label || "smth"}
+            </li>
+          );
+        })}
+      </ul>
+      {selectedDevice && (
+        <Webcam audio={false} videoConstraints={{ ...videoConstraints, deviceId: selectedDevice?.deviceId }} />
+      )}
+    </div>
   );
 }
 
